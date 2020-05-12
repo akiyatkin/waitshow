6bb5bb6550e551d2ac9850183367135ef6d8000d
Anton Kiyatkin <a.kiyatkin@itlf.ru>
2020-05-12 23:45:32 +0400

    Fix

--- infra.js
+++ infra.js
@@ -1,21 +1,20 @@
 import { CDN } from '/vendor/akiyatkin/load/CDN.js'
 import { Waitshow } from '/vendor/akiyatkin/waitshow/Waitshow.js'
-import { Event } from "/vendor/infrajs/event/Event.js"
 import { DOM } from '/vendor/akiyatkin/load/DOM.js'
 
 let tag = tag => document.getElementsByTagName(tag)
 
-DOM.wait('show').then(() => {
-    window.addEventListener('scroll', Waitshow)
-    Event.handler('Controller.onshow', () => {
-        for (let img of tag('img')) {
-            let path = [], el = img
-            while (el && el.parentElement) path.push(el = el.parentElement)
-            let ar = path.filter(el => ~['HEADER','FOOTER'].indexOf(el.tagName))
-            if (ar.length) continue //Для картинок в шапке подвале ничего не добавляем
-            img.classList.add('waitshow')
-        }
-        Waitshow()
-    })
-    CDN.css('waitshow', '/vendor/akiyatkin/waitshow/style.css')
+window.addEventListener('scroll', Waitshow)
+
+DOM.race('show', () => {
+    for (let img of tag('img')) {
+        let path = [], el = img
+        while (el && el.parentElement) path.push(el = el.parentElement)
+        let ar = path.filter(el => ~['HEADER','FOOTER'].indexOf(el.tagName))
+        if (ar.length) continue //Для картинок в шапке подвале ничего не добавляем
+        img.classList.add('waitshow')
+    }
+    Waitshow()
 })
+
+CDN.css('waitshow', '/vendor/akiyatkin/waitshow/style.css')
