4381b57fe1e9c3607ce79b0082d04e6a172be689
Anton Kiyatkin <a.kiyatkin@itlf.ru>
2020-07-18 17:11:26 +0400

    fix

--- CallFrame.js
+++ CallFrame.js
@@ -2,13 +2,13 @@
     Повторно передаать функцию для запуска нужно в тойже переменной. 
     Некоторые запуски будут пропускаться "схлопываться". С Timeout и в animationFrame
 */
-let CallFrame = (callback) => {
+let CallFrame = (callback, t = 200) => {
     if (CallFrame.store.has(callback)) return;
     CallFrame.store.add(callback)
     setTimeout(() => requestAnimationFrame(() => {
         callback()
         CallFrame.store.delete(callback)
-    }), 200)
+    }), t)
 }
 CallFrame.store = new Set()
 
